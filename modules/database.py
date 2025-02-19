1d10245abc39a6843b61b5a4199b1c825e873204
hiro-v1 <mamanteng13@gmail.com>
2025-02-19 08:59:06 +0700

    Update database.py

--- modules/database.py
+++ modules/database.py
@@ -1,14 +1,13 @@
 import pymongo
 import os
 
-# Koneksi MongoDB
 client = pymongo.MongoClient(os.getenv("MONGO_URI"))
 db = client["hirokesbot"]
 
 def add_banned_word(word):
     """Menambahkan kata terlarang ke database"""
-    db["banned_words"].insert_one({"word": word})
+    db.banned_words.insert_one({"word": word.lower()})
 
 def remove_banned_word(word):
-    """Menghapus kata dari daftar terlarang"""
-    db["banned_words"].delete_one({"word": word})
+    """Menghapus kata terlarang dari database"""
+    db.banned_words.delete_one({"word": word.lower()})

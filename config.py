97fa3366633d5f4bf2735a0ef86bcab2239a843f
hiro-v1 <mamanteng13@gmail.com>
2025-02-26 09:11:22 +0700

    Update config.py

--- config.py
+++ config.py
@@ -8,5 +8,4 @@ API_HASH = os.getenv("API_HASH")
 BOT_TOKEN = os.getenv("BOT_TOKEN")
 OWNER_ID = 5432983527  # Pemilik bot tetap
 MONGO_URI = os.getenv("MONGO_URI")
-HUGGINGFACE_MODEL = os.getenv("HUGGINGFACE_MODEL", "cahya/gpt2-small-indonesian-522M")
-HUGGINGFACE_API_KEY = os.getenv("HUGGINGFACE_API_KEY")
+OPENAI_API_KEY = os.getenv("OPENAI_API_KEY")
